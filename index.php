651caba09de361c0aab650fbb441716bfc936c41
Michael <contact@eewee.fr>
2019-10-15 23:37:19 +0200

    Update summary.

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <h1>PHP :</h1>
-<a href="etape01.php">Etape 01 - variable</a><br>
+<ul>
+    <li><a href="etape01.php">Etape 01 - variable et tableau</a></li>
+    <li><a href="etape02.php">Etape 02 - condition</a></li>
+    <li><a href="etape03.php">Etape 03 - boucle</a></li>
+</ul>
