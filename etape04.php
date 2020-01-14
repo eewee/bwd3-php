93a453b8f7d519b0244281210c6a37f662cb9929
Michael <contact@eewee.fr>
2020-01-14 15:19:52 +0100

    Add functions.

--- etape04.php
+++ etape04.php
@@ -6,11 +6,50 @@
             <div class="col-8">
 
                 <div class="alert alert-primary" role="alert">
-                    <h2>$_GET :</h2>
+                    <h2>Function :</h2>
                 </div>
 
                 <?php
+                //---------------------------------------------------------------------------
+                // FUNCTIONS :
+                //---------------------------------------------------------------------------
 
+                function bonjour($prenom, $nom) {
+                    echo "Bonjour ".$prenom." ".$nom;
+                }
+
+                // Déclaration de la fonction :
+                function getStudentById($eleves, $id=2) {
+                    return $eleves[$id];
+                }
+
+                //---------------------------------------------------------------------------
+                // INIT DONNEES :
+                //---------------------------------------------------------------------------
+
+                // Votre tableau :
+                $eleves = array(
+                    array('nom'=>'DURANT', 'prenom'=>'Julien', 'email'=>'julien@mailinator.com', 'age'=>22),
+                    array('nom'=>'LADY',   'prenom'=>'Sophie', 'email'=>'sophie@mailinator.com', 'age'=>25),
+                    array('nom'=>'DULON',  'prenom'=>'Pierre', 'email'=>'pierre@mailinator.com', 'age'=>20)
+                );
+
+                //---------------------------------------------------------------------------
+                // UTILISER LES FONCTIONS :
+                //---------------------------------------------------------------------------
+
+                bonjour( "John", "DOE" );
+                echo "<hr>";
+
+                // Call fonction :
+                $monEleve = getStudentById($eleves);
+
+                // Affichage (debug only) :
+                echo '<pre>'.var_export($monEleve, true).'</pre>';
+
+                // Affichage de l'email :
+                echo "<p>Mon email est : ".$monEleve['email']."</p>";
+                echo "<hr>";
                 ?>
 
             </div>
