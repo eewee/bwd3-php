d2d1715dd8986e776b5fb8761a0529651c16dbfd
Michael <contact@eewee.fr>
2020-01-14 12:28:40 +0100

    Add conditions.

--- etape02.php
+++ etape02.php
@@ -9,8 +9,63 @@
                     <h2>Conditions :</h2>
                 </div>
 
-                <?php
 
+
+
+                <?php
+                $feuTricolor = "vert";
+                $wording = [
+                    "vert"   => "Vous devez passer",
+                    "orange" => "Vous devez essayer de vous arrêter (si pas le temps, continuer)",
+                    "rouge"  => "Vous devez vous arrêter",
+                    "autre"  => "Vous devez suivre les panneaux"
+                ];
+                //echo "<pre>".var_export($wording, true)."</pre>";
+
+
+
+
+                echo "<h2>If / elseif / else (=Faire une action Si / Sinon):</h2>";
+                if ($feuTricolor == "vert") {
+                    $quoiFaire = $wording[$feuTricolor];
+
+                } elseif ($feuTricolor == "orange") {
+                    $quoiFaire = $wording[$feuTricolor];
+
+                } elseif ($feuTricolor == "rouge") {
+                    $quoiFaire = $wording[$feuTricolor];
+
+                } else {
+                    $quoiFaire = $wording[$feuTricolor];
+                }
+                echo "Le feu est : ".$feuTricolor."<br>";
+                echo "Quoi faire : ".$quoiFaire."<br>";
+                echo "<hr>";
+
+
+
+
+                echo "<h2>Switch :</h2>";
+
+                switch($feuTricolor) {
+                    case "vert" :
+                        $quoiFaire = $wording["vert"];
+                        break;
+
+                    case "orange" :
+                        $quoiFaire = $wording["orange"];
+                        break;
+
+                    case "rouge" :
+                        $quoiFaire = $wording["rouge"];
+                        break;
+
+                    default :
+                        $quoiFaire = $wording["autre"];
+                }
+                echo "Le feu est : ".$feuTricolor."<br>";
+                echo "Quoi faire : ".$quoiFaire."<br>";
+                echo "<hr>";
                 ?>
 
             </div>
